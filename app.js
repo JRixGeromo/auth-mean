ef6b4aedb21f6c69c8b54c8a56442fe3377b7f2c
JRixGeromo <jrixgeromo@gmail.com>
2017-11-13 09:28:18 +0800

    for heroku fix10

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var userRoutes = require('./routes/user');
 var app = express();
 //mongoose.connect('mongodb://localhost:27017/node-angular', { useMongoClient: true })
 
-mongoose.connect('mongodb://jrix:jrix123@ds259325.mlab.com:59325/jintelauth', { useMongoClient: true });
+mongoose.connect('jrix:jrix123@ds259325.mlab.com:59325/jintelauth');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
